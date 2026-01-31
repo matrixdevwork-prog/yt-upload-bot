563a7021532b865adf7e217b82bafdefaa3643b7
matrixdevwork-prog <matrixdevwork@gmail.com>
2026-01-31 14:28:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("YT automation bot is alive")
+import os
+import json
+
+print("Bot started")
+
+oauth_json = os.getenv("GOOGLE_OAUTH_JSON")
+if not oauth_json:
+    raise Exception("OAuth secret not found")
+
+creds = json.loads(oauth_json)
+print("OAuth credentials loaded")
+print("Client ID:", creds.get("client_id", "ok"))
+
+print("Dry run successful")
+
 
